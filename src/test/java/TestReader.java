9967d3d5bd28dbe35e26561ff9244852271b1b0f
Lee R. Boynton <lee@boynton.com>
2014-05-24 19:05:47 -0700

    Switched from TestNG to JUnit
    and reformatted code

--- src/test/java/TestReader.java
+++ src/test/java/TestReader.java
@@ -1,6 +1,6 @@
-import org.testng.annotations.Test;
-import org.testng.Assert;
-import static org.testng.Assert.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.jelled.Ell.*;
 import static org.jelled.Runtime.*;
 import java.io.FileReader;
 
@@ -14,7 +14,7 @@ public class TestReader {
             var sdata = write(data);
             var data2 = read(sdata);
             boolean b = equal(data, data2);
-            Assert.assertTrue(b);
+            assertTrue(b);
         } catch (error e) {
             System.out.println("fail!");
             e.printStackTrace();
@@ -39,25 +39,25 @@ public class TestReader {
     public void testTypes() {
         try {
             var data = read(file_contents(file("./src/test/resources/test1.elldn")));
-			println(data);
+            println(data);
             var v = get(data, "nothing");
-			assertTrue(v == NIL);
+            assertTrue(v == NIL);
             v = get(data, keyword("k"));
-			assertTrue(type(v) == SYM_KEYWORD);
+            assertTrue(type(v) == SYM_KEYWORD);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-	@Test
-	public void testReadWriteString() {
-		var d1 = map(intern("x"), "symbol", keyword("y"), "keyword", string("z"), "string");
-		var sdata = write(d1);
-		var d2 = read(sdata);
-		assertTrue(equal(d1, d2));
-	}
+    @Test
+    public void testReadWriteString() {
+        var d1 = map(intern("x"), "symbol", keyword("y"), "keyword", string("z"), "string");
+        var sdata = write(d1);
+        var d2 = read(sdata);
+        assertTrue(equal(d1, d2));
+    }
 
-	@Test
+    @Test
     public void testReadWrite() {
         try {
             println("begin testReadWrite");
@@ -68,22 +68,22 @@ public class TestReader {
                 assertTrue(isNil(get(data, intern("ary"))));
                 assertTrue(!isNil(get(data, keyword("ary"))));
                 var bad_data = read(channel);
-				println(bad_data);
+                println(bad_data);
                 var sdata = write(data);
                 println(sdata);
                 var data2 = read(sdata);
-				Assert.assertTrue(equal(data, data2));
-				try {
-					read(string(""));
-					Assert.fail("reading an empty string should throw an error");
-				} catch (error e) {
-				}
-	            var outchan = open(file("/tmp/test1.elldn"), WRITE);
-				write(data, outchan);
-				close(outchan);
+                assertTrue(equal(data, data2));
+                try {
+                    read(string(""));
+                    fail("reading an empty string should throw an error");
+                } catch (error e) {
+                }
+                var outchan = open(file("/tmp/test1.elldn"), WRITE);
+                write(data, outchan);
+                close(outchan);
             } catch (Throwable e) {
                 e.printStackTrace();
-                Assert.fail("Whoops: " + e);
+                fail("Whoops: " + e);
             } finally {
                 println("closing...");
                 close(channel);
